670ab3c79a50a1aa2f9cf0e0d91d2212749a91c5
xTheCoderx <justaguyoney@gmail.com>
2021-10-17 18:03:59 +1300

    Set bindings in code.

--- app/src/main/java/com/realtomjoney/pixels_and_dps/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/pixels_and_dps/MainActivity.kt
@@ -2,10 +2,19 @@ package com.realtomjoney.pixels_and_dps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.realtomjoney.pixels_and_dps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setBindings() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
